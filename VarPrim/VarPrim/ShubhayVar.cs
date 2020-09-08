9eec62a9fd07d75bf427c7ec3e6a350c6eee4738
shubhayshah <65247497+shubhayshah@users.noreply.github.com>
2020-09-07 19:35:56 -0700

    Add files via upload
    Adds all neccesary files. 
    
    WILL NEED VS 2019 TO RUN

---
+++ VarPrim/VarPrim/ShubhayVar.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarPrim
+{
+    class ShubhayVar
+    {
+        static void Main(string[] args)
+        {
+            /// Declares a variable and prompts user to type in number 
+            string anotherval; 
+            /// Declares data types used in out later in the if and else if statements
+            sbyte rese;
+            byte resolution;
+            short middleone;
+            ushort type_;
+            int hellobro;
+            uint soarer;
+            long longtype;
+            ulong uversionlong;
+            float floattype;
+            double doubletype;
+            decimal decimaltype;
+
+            /// Gives back the result 
+            /// This program leaves out object and string (both null), bool and char (t/f and character)
+
+            do
+            {
+                Console.WriteLine("Enter a number!! - ");
+                var val = Console.ReadLine();
+
+                if (SByte.TryParse(val, out rese))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a SByte!");
+                }
+                else if (Byte.TryParse(val, out resolution))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a Byte!");
+                }
+                else if (short.TryParse(val, out middleone))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a Short!");
+                }
+                else if (ushort.TryParse(val, out type_))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a UShort!");
+                }
+                else if (int.TryParse(val, out hellobro))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a Int!");
+                }
+                else if (uint.TryParse(val, out soarer))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a UInt!");
+                }
+                else if (long.TryParse(val, out longtype))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a Long!");
+                }
+                else if (ulong.TryParse(val, out uversionlong))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a ULong!");
+                }
+                else if (float.TryParse(val, out floattype))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a Float!");
+                }
+                else if (double.TryParse(val, out doubletype))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a Double!");
+                }
+                else if (decimal.TryParse(val, out decimaltype))
+                {
+                    Console.WriteLine("This means that your number, " + val + " is a Decimal!");
+                }
+
+                Console.WriteLine("Do you want to run the program again? Yes or no?");
+                anotherval = Console.ReadLine();
+
+            } while (anotherval != "no");
+
+            Console.WriteLine("Good Bye.");
+
+            /// End of program 
+        }
+    }
+}
---
+++ VarPrim/VarPrim/VariablesAndPrimitives.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This is a basic program explaining datatypes 
+// Hint, hint -- talking about the byte type
+
+namespace VarPrim
+{
+    class VariablesAndPrimitives
+    {
+        static void Main2(string[] args)
+        {
+            // There are many different types of [integer] types such as
+            /// sbyte , Def. Value is 0, whereas min. value is -128 and max. value is 127
+
+
+            /// byte, Def. and Min. Values are 0; Max. Value is 255
+            sbyte MurrowNull = 5;
+            byte ByteDataType = 10;
+            int IntegerTypeOne = 126;
+            var TestForFunction = 162; 
+
+
+
+            /// Writes out 'This is a byte function' on the console 
+            Console.WriteLine("These are all data types, right? The sbyte value is " + MurrowNull + " and ByteDataType's value is " + ByteDataType);
+            Console.WriteLine("Integers are the most popular data type. The value is " + IntegerTypeOne);
+            Console.WriteLine("Variables are the last topic for today! The value is " + TestForFunction + " !");
+
+            /// This prints out the maximum value of the 64-bit Int data type 
+            /// The value is 18446744073709551615
+
+            ulong maxIntValue = UInt64.MaxValue;
+            Console.WriteLine("The value of the 64-bit Int is " + maxIntValue);
+
+        }
+    }
+}
+
+
+/* The byte type specifies the set of integers in the range of 0 to 255. 
+ The byte type is mainly used for memory and storage, as the byte type is
+considered as a Data Type */
+
+
